920898df2803b17f197259ecf469c1ab3f6a2d3e
Volodymyr K <kvladimir2007@gmail.com>
2017-04-05 16:46:16 +0300

    Use Url from properties

--- Assignment2/src/main/java/myprojects/automation/assignment2/tests/CheckMainMenuTest.java
+++ Assignment2/src/main/java/myprojects/automation/assignment2/tests/CheckMainMenuTest.java
@@ -2,6 +2,7 @@ package myprojects.automation.assignment2.tests;
 
 import myprojects.automation.assignment2.BaseScript;
 import myprojects.automation.assignment2.utils.Helper;
+import myprojects.automation.assignment2.utils.Properties;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,13 +12,12 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class CheckMainMenuTest extends BaseScript {
-    private static String BASE_URL = "http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/";
-    private static int TIMEOUT = 2000;
+    // private static int TIMEOUT = 2000;
     private static WebDriver driver = null;
 
     public static void main(String[] args) throws InterruptedException {
         // Script to check Main Menu items
-        driver = Helper.open(BASE_URL);
+        driver = Helper.open(Properties.getBaseAdminUrl());
 
         Helper.signIn(driver, "webinar.test@gmail.com","Xcg7299bnSmMuRLp9ITw");
 
--- Assignment2/src/main/java/myprojects/automation/assignment2/tests/LoginTest.java
+++ Assignment2/src/main/java/myprojects/automation/assignment2/tests/LoginTest.java
@@ -2,18 +2,17 @@ package myprojects.automation.assignment2.tests;
 
 import myprojects.automation.assignment2.BaseScript;
 import myprojects.automation.assignment2.utils.Helper;
+import myprojects.automation.assignment2.utils.Properties;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class LoginTest extends BaseScript {
-    private static String BASE_URL = "http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/";
 
+    // Script to execute login and logout steps
     public static void main(String[] args) throws InterruptedException {
-        // Script to execute login and logout steps
-
-        WebDriver driver = Helper.open(BASE_URL);
+        WebDriver driver = Helper.open(Properties.getBaseAdminUrl());
 
         Helper.signIn(driver, Helper.Nickname, Helper.Password);
 
